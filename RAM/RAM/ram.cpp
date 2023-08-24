7e1e519bbffac61e4955f557e416923121ca868b
Котов Иван <kotov038@gmail.com>
2023-08-24 11:51:22 +0300

    Initial commit

---
+++ RAM/RAM/ram.cpp
@@ -0,0 +1,77 @@
+﻿#include "ram.hpp"
+
+#include <fstream>
+#include <bitset>
+
+ram::ram()
+{
+    std::ranges::fill(_data, 0);
+}
+
+std::optional<ram::mem> ram::load_file()
+{
+    ram::mem data;
+    static std::regex reg(R"(([0-9a-fA-F]+)\t([0-9a-fA-F]+))");
+
+    std::ifstream file(FILE_NAME, std::ios::in | std::ios::binary);
+    if (!file)
+        return {};
+
+    std::string line;
+    std::smatch reg_match;
+    size_t i = 0;
+    for (; std::getline(file, line); )
+    {
+        if (!std::regex_match(line, reg_match, reg))
+            continue;
+
+        auto addr = std::stoi(reg_match[1], nullptr, 16);
+        addr <<= (sizeof(addr) * 8 - ADDR_SIZE);
+        addr >>= (sizeof(addr) * 8 - ADDR_SIZE);
+
+        data[i] = addr;
+        data[i] <<= 28;
+        data[i] += std::stoi(reg_match[2], nullptr, 16);
+
+        if (++i; i >= MEM_SIZE)
+            break;
+    }
+
+    return data;
+}
+
+VOID ram::setup(IINSTANCE *instance, IDSIMCKT *dsim)
+{
+    _instance = instance;
+
+    vsm::model::init_pins(_instance, _pins_D, "D");
+    vsm::model::init_pins(_instance, _pins_A, "A");
+    vsm::model::init_pins(_instance, _pins_C, "C");
+
+    auto file_data = load_file();
+    if (file_data.has_value())
+        std::swap(_data, file_data.value());
+}
+
+VOID ram::simulate(ABSTIME time, DSIMMODES mode)
+{
+    auto addr = vsm::model::make_number(_pins_A);
+    std::bitset<sizeof(mem::value_type) * 8> bits(_data[addr]);
+    for (size_t i = 0; i < _pins_C.size(); ++i)
+    {
+        _pins_C[i].set(time, 50000, (bits[bits.size() - i - 1] == 1 ? SHI : SLO));
+    }
+}
+
+extern "C"
+{
+    IDSIMMODEL __declspec(dllexport) *createdsimmodel(CHAR *device, ILICENCESERVER *license_server)
+    {
+        return license_server->authorize(ram::MODEL_KEY) ? new ram : nullptr;
+    }
+
+    VOID __declspec(dllexport) deletedsimmodel(IDSIMMODEL *model)
+    {
+        delete static_cast<ram *>(model);
+    }
+}
---
+++ RAM/RAM/ram.hpp
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include <model.hpp>
+#include <pin.hpp>
+
+#include <array>
+#include <optional>
+#include <string>
+#include <ranges>
+#include <regex>
+
+class ram final : public vsm::model
+{
+public:
+    static constexpr DWORD MODEL_KEY = 0x00000001;
+
+private:
+    static constexpr uint8_t ADDR_SIZE = 4;
+    static constexpr uint8_t MEM_SIZE = 16;
+
+    static constexpr const char *FILE_NAME = "out.bin";
+
+    using mem = std::array<uint32_t, MEM_SIZE>;
+    using in_pins = std::array<vsm::pin, ADDR_SIZE>;
+    using out_pins = std::array<vsm::pin, ADDR_SIZE + 28>;
+
+private:
+    in_pins _pins_D;
+    in_pins _pins_A;
+    out_pins _pins_C;
+
+    mem _data;
+
+public:
+    std::optional<mem> load_file();
+
+public:
+    ram();
+
+    VOID setup(IINSTANCE *instance, IDSIMCKT *dsim) override;
+
+    VOID simulate(ABSTIME time, DSIMMODES mode) override;
+
+    ~ram() = default;
+};
